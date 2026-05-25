82ea8f2af2acb7080566d767393b05e378ef8cb4
LOQ GAMING <hanchantrea38@gmail.com>
2026-05-25 09:18:07 +0700

    create the dto validation feature/dto-validation

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,18 +4,19 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule } from '@nestjs/config';
 
-import { AuthController } from '../controllers/auth/auth.controller';
-import { AuthService } from '../services/auth/auth.service';
-import { AuthRepository } from '../repositories/auth/auth.repository';
-import { AuthToken } from '../entities/auth/auth-token.entity';
+import { AuthController } from '../../controllers/auth/auth.controller';
+import { AuthService } from '../../services/auth/auth.service';
+import { AuthRepository } from '../../repositories/auth/auth.repository';
+import { AuthToken } from '../../entities/auth/auth-token.entity';
 
-import { UserModule } from './user/user.module';
-import { jwtConfig } from '../config';
+import { UserModule } from '../user/user.module';
+import { jwtConfig } from '../../config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
-    PassportModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: jwtConfig.secret,
       signOptions: { expiresIn: jwtConfig.expiresIn },
