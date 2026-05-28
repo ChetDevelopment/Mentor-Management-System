3367e0a5859898c7434f08b27be9ddcbd24f7010
LOQ GAMING <hanchantrea38@gmail.com>
2026-05-28 16:43:59 +0700

    Implement User module: repository, service, controller, DTOs with guards and validation

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -1,20 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminController } from '../../controllers/admin/admin.controller';
 import { AdminService } from '../../services/admin/admin.service';
 import { UserModule } from '../user/user.module';
 import { MentorModule } from '../mentor/mentor.module';
 import { MenteeModule } from '../mentee/mentee.module';
-import { UserRepository } from '../../repositories/user/user.repository';
-import { MentorRepository } from '../../repositories/mentor/mentor.repository';
-import { MenteeRepository } from '../../repositories/mentee/mentee.repository';
-import { User } from '../../entities/user/user.entity';
-import { Mentor } from '../../entities/mentor/mentor.entity';
-import { Mentee } from '../../entities/mentee/mentee.entity';
 
 @Module({
   imports: [UserModule, MentorModule, MenteeModule],
   controllers: [AdminController],
-  providers: [AdminService, UserRepository, MentorRepository, MenteeRepository],
+  providers: [AdminService],
 })
 export class AdminModule {}
