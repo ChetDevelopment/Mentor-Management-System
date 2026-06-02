8a0e6620f91ce3a2b7bfaa73e763b4261f48f50b
ChetDevelopment <vichet.sat@student.passerellesnumeriques.org>
2026-06-02 09:03:56 +0700

    feat: implement skill module with Category relation and Mentor ManyToMany

--- src/modules/skill/skill.module.ts
+++ src/modules/skill/skill.module.ts
@@ -4,9 +4,10 @@ import { SkillController } from '../../controllers/skill/skill.controller';
 import { SkillService } from '../../services/skill/skill.service';
 import { SkillRepository } from '../../repositories/skill/skill.repository';
 import { Skill } from '../../entities/skill/skill.entity';
+import { Category } from '../../entities/category/category.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Skill])],
+  imports: [TypeOrmModule.forFeature([Skill, Category])],
   controllers: [SkillController],
   providers: [SkillService, SkillRepository],
   exports: [SkillService],
